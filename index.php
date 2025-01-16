5d8a687d4ad4348dae965a0ad18bf9f1741b6b7c
ElGibran17 <ibrahimelgibran17@gmail.com>
2025-01-16 22:14:59 +0700

    update

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ require_once 'function/constant.php';
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <!-- Open Graph -->
+  <!-- Open Graph  -->
   <meta property="og:title" content="<?= getName(); ?> - Home" />
   <meta name="author" content="<?= getName(); ?>" />
   <meta property="og:locale" content="id" />
